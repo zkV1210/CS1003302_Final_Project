b2e6cf68c8dc82100bff7fc4017d96df2c7d15f3
zkV1210 <kevin20160205@gmail.com>
2022-12-21 22:42:19 +0800

    Xfinal

--- project.c
+++ project.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int arrayinput(int arrayf[][6],int arrayb[][6],int in[],int len);
 int arraystandardization(int array[],int len);
@@ -8,45 +10,63 @@ int computing(int ans[],int len);
 int checkCK(int ans[],int len);
 
 int main(){
+    FILE *file;
+    file = fopen( "test.txt", "r+" );
+    char lenstring[10]={0},*trash,widthstring[15][100];
     int widthsf[25][6],widthsb[25][6],in[150],ansf[25],ansb[25];
-    int length = 1, input = 0,f=0,b=0;
-    for (int i = 0;i<25;i++){
-        for (int n = 0;n<6;n++){
-            widthsf[i][n] = -1;
-            widthsb[i][n] = -1;
-        }
-    }
-    for (int i = 0;i<25;i++){
-        ansf[i] = -1;
-        ansb[i] = -1;
-    }
-    scanf("%d",&length);
-    
-    input = arrayinput(widthsf,widthsb,in,length);
-
-    if (input==0){
-        f = decoding(widthsf,length,ansf);
-        b = decoding(widthsb,length,ansb);
-        if (f==1&&b==1){
-            printf("bad code\n");
-        }else{
-            f = computing(ansf,length);
-            b = computing(ansb,length);
-            if (f==b&&f==1){
-                printf("bad code\n");
+    int length = 1, input = 0,f=0,b=0,casei=1;
+    while(length!=0){
+        for (int i = 0;i<25;i++){
+            for (int n = 0;n<6;n++){
+                widthsf[i][n] = -1;
+                widthsb[i][n] = -1;
             }
         }
-    }else {
-        printf("bad code\n");
+        for (int i = 0;i<25;i++){
+            ansf[i] = -1;
+            ansb[i] = -1;
+        }
+        fgets(lenstring,10,file);
+        length = strtol(lenstring,&trash,0);
+        if(length==0){
+            break;
+        }
+        printf("Case %d: ",casei);
+        if (length%10!=0){
+            for (int i = 0;i<length/10+1;i++){
+                fgets(widthstring[i],100,file);
+                in[i*10]=strtol(widthstring[i],&trash,0);
+                for (int n = 1;n<10;n++){
+                    if (i*10+n<length){
+                        in[i*10+n]=strtol(trash,&trash,0);
+                    }
+                }
+            }
+        }
+        input = arrayinput(widthsf,widthsb,in,length);
+        if (input==0){
+            f = decoding(widthsf,length,ansf);
+            b = decoding(widthsb,length,ansb);
+            if (f==1&&b==1){
+                printf("bad code\n");
+            }else{
+                f = computing(ansf,length);
+                b = computing(ansb,length);
+                if (f==b&&f==1){
+                    printf("bad code\n");
+                }
+            }
+        }else {
+            printf("bad code\n");
+        }
+        casei++;
     }
+    fclose(file);
 }
 
 int arrayinput(int arrayf[][6],int arrayb[][6],int in[],int len){
     int arraystandardization(int array[],int len);
     int now = 0, stdz=0;;
-    for (int i =0;i<len;i++){
-        scanf("%d",&in[i]);
-    }
     stdz = arraystandardization(in,len);
     if (stdz){
         return 1;
@@ -71,18 +91,21 @@ int arrayinput(int arrayf[][6],int arrayb[][6],int in[],int len){
     return 0;
 }
 int arraystandardization(int array[],int len){
-    float narrow = 0,a=0,b=0;
+    float narrow = 0,a=0,b=0,count = 0;
     int now =0;
     while (narrow==0){
         a = array[now];
         b = array[now+1];
-        if (a*2.05>b&&a*1.95<b){
+        if (a*2.1>b&&a*1.9<b){
             narrow = a;
-        }else if (b*2.05>a&&b*1.95<a){
+        }else if (b*2.1>a&&b*1.9<a){
             narrow = b;
         }
         now++;
-        if (now==6){
+        if (array[now]==narrow){
+            count++;
+        }
+        if (now==6&&count<2){
             return 1;
         }
     }
@@ -91,7 +114,7 @@ int arraystandardization(int array[],int len){
             array[i] = 0;
         }else if(array[i]<=narrow*2.1&&array[i]>=narrow*1.9){
             array[i] = 1;
-        }else{
+        }else {
             return 1;
         }
     }
@@ -130,9 +153,7 @@ int decoding(int array[][6],int len,int ans[]){
         }else {
             return 1;
         }
-        printf("%d ",ans[i]);
     }
-    printf("\n");
     return 0;
 }
 int computing(int ans[],int len){
@@ -153,7 +174,6 @@ int computing(int ans[],int len){
         return 1;
     }
 }
-
 int checkCK(int ans[],int len){
     int C2=0,C3=0,C4=0,Clong=0,K2=0,K3c=0,K3k=0,K4=0,K3=0;
     for (int i = 1;i<len-2;i++){
@@ -168,20 +188,17 @@ int checkCK(int ans[],int len){
             K3=K3+((len-3-i)%9+1)*ans[i];
         }
     }
-    K2=(K2+ans[len-2])%11;
-    K3c=(K3+10)%11;
-    K3k=(K3+ans[len-3])%11;
-    K4=(K4+10)%11;
+    K2=(K2+C2%11)%11;
+    K3 = (K3+C3%11)%11;
+    K4=(K4+C4%11)%11;
     C2=C2%11;
     C3=C3%11;
     C4=C4%11;
-    printf("C:%d %d %d\n",C2,C3,C4);
-    printf("K:%d %d %d %d\n",K2,K3c,K3k,K4);
     if(C2==ans[len-2]&&K2==ans[len-1]){
         return 2;
-    }else if(C3==10&&ans[len-2]==0&&ans[len-3]==1&&K3c==ans[len-1]){
+    }else if(C3==10&&ans[len-2]==0&&ans[len-3]==1&&K3==ans[len-1]){
         return 3;
-    }else if(K3k==10&&ans[len-1]==0&&ans[len-2]==1&&C3==ans[len-3]){
+    }else if(K3==10&&ans[len-1]==0&&ans[len-2]==1&&C3==ans[len-3]){
         return 3;
     }else if(C4==10&&K4==10&&ans[len-3]==0&&ans[len-4]==1&&ans[len-1]==0&&ans[len-2]==1){
         return 4;
@@ -189,9 +206,11 @@ int checkCK(int ans[],int len){
         return 1;
     }else if(C3==10&&ans[len-2]==0&&ans[len-3]==1){
         return 1;
-    }else if(C3==ans[len-3]){
+    }else if(C3==ans[len-3]&&ans[len-1]==0&&ans[len-2]==1){
         return 1;
-    }else{
+    }else if(C4==10&&ans[len-3]==0&&ans[len-4]==1&&ans[len-1]==0&&ans[len-2]==1){
+        return 1;
+    }else {
         return 0;
     }
 }
